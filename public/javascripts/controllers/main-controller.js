8bad080fa867575be2f09a8ef8d044922553341d
Khalil <knb6@njit.edu>
2015-06-19 22:17:20 -0400

    removed carousel

--- public/javascripts/controllers/main-controller.js
+++ public/javascripts/controllers/main-controller.js
@@ -60,16 +60,16 @@ function($scope, posts, auth,$modal,$log,$geolocation,$http,location){
 
   $scope.myInterval = 5000;
   var slides = $scope.slides = [];
-  $scope.addSlide = function() {
-    var newWidth = 600 + slides.length + 1;
-    slides.push({
-      image: 'http://placekitten.com/' + newWidth + '/300',
-      text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
-        ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
-    });
-  };
-  for (var i=0; i<4; i++) {
-    $scope.addSlide();
-  }
+  // $scope.addSlide = function() {
+  //   var newWidth = 600 + slides.length + 1;
+  //   slides.push({
+  //     image: 'http://placehold.it/' + newWidth + 'x300',
+  //     text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+  //       ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+  //   });
+  // };
+  // for (var i=0; i<4; i++) {
+  //   $scope.addSlide();
+  // }
 
 }])
