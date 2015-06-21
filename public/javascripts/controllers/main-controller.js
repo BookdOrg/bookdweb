a8e7e3becce2e42485ba464df8e85640b7b443a3
Khalil <knb6@njit.edu>
2015-06-21 18:40:28 -0400

    temp footer fix

--- public/javascripts/controllers/main-controller.js
+++ public/javascripts/controllers/main-controller.js
@@ -64,16 +64,5 @@ function($scope, posts, auth,$modal,$log,$geolocation,$http,location){
 
   $scope.myInterval = 5000;
   var slides = $scope.slides = [];
-  // $scope.addSlide = function() {
-  //   var newWidth = 600 + slides.length + 1;
-  //   slides.push({
-  //     image: 'http://placehold.it/' + newWidth + 'x300',
-  //     text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
-  //       ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
-  //   });
-  // };
-  // for (var i=0; i<4; i++) {
-  //   $scope.addSlide();
-  // }
 
 }])
