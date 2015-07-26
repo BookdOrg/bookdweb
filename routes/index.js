8874e7d7098914f597953d4140ded0041327f938
Khalil <knb6@njit.edu>
2015-07-26 01:52:31 -0400

    updates

--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,7 @@ io.on('connection',function(socket){
   });
 });
 
-server.listen(process.env.devlocalPort);
+// server.listen(process.env.devlocalPort);
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: '' });
