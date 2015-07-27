1dc61f3e94591cbcac2a4349ea0a05ef58e5a47b
Khalil <knb6@njit.edu>
2015-07-27 12:57:09 -0400

    UI quick fi

--- public/javascripts/controllers/bizlist-controller.js
+++ public/javascripts/controllers/bizlist-controller.js
@@ -5,17 +5,21 @@ angular.module('cc.bizlist-controller',[])
 '$state',
 'location',
 'yelpService',
-'$stateParams',
 'businesses',
 'location',
 '$rootScope',
-function($scope, auth, $state,location,$stateParams,yelpService,businesses,location,$rootScope){
-	$scope.cat = $stateParams.cat;
+function($scope, auth, $state,location,yelpService,businesses,location,$rootScope){
 	$scope.businesses = businesses.data.businesses;
-	
-	for(var i=0; i<$scope.businesses.length; i++){
-		$scope.businesses[i].distance = location.calculateDistance($scope.businesses[i].location.coordinate.longitude,
-			$scope.businesses[i].location.coordinate.latitude, $rootScope.currLocation.lng,$rootScope.currLocation.lat);
-	}
+	$scope.cat = $scope.businesses[0].categories[0][0];
+	console.log($scope.cat)
+		for(var i=0; i<$scope.businesses.length; i++){
+			if($rootScope.currLocation){
+				$scope.businesses[i].distance = location.calculateDistance($scope.businesses[i].location.coordinate.longitude,
+				$scope.businesses[i].location.coordinate.latitude, $rootScope.currLocation.lng,$rootScope.currLocation.lat);	
+			}else{
+				$scope.businesses[i].distance = "N/A";
+			}
+			
+		}
 
 }])
