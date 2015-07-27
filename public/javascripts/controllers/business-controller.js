36b8fd6bb63f6bfa58e0762969f639295b7668d3
Khalil <knb6@njit.edu>
2015-07-27 19:26:50 -0400

    working on editing business page

--- public/javascripts/controllers/business-controller.js
+++ public/javascripts/controllers/business-controller.js
@@ -9,7 +9,8 @@ angular.module('cc.business-controller',[])
 'businessFactory',
 'location',
 '$rootScope',
-function($scope, auth, $state,location,$stateParams,business,businessFactory,location,$rootScope){
+'$modal',
+function($scope, auth, $state,location,$stateParams,business,businessFactory,location,$rootScope,$modal){
 	$scope.currentUser = auth.currentUser();
 	$scope.business = business.data;
 
@@ -18,9 +19,33 @@ function($scope, auth, $state,location,$stateParams,business,businessFactory,loc
 	businessFactory.getBusiness($scope.business.id);
 	$scope.lbusiness = businessFactory.business;
 
+	$scope.animationsEnabled = true;
+
+  $scope.open = function (size) {
+
+    var modalInstance = $modal.open({
+      animation: $scope.animationsEnabled,
+      templateUrl: 'addServiceModal.html',
+      controller: 'addServiceModalCtrl',
+      size: size
+    });
+   };
+  $scope.toggleAnimation = function () {
+    $scope.animationsEnabled = !$scope.animationsEnabled;
+  };
 	// if($scope.currentUser._id === $scope.lbusiness.owner._id){
 	// 	$scope.canEdit = true;
 	// }else{
 	// 	$scope.canEdit = false;
 	// }
 }])
+.controller('addServiceModalCtrl', function ($scope, $modalInstance) {
+
+  $scope.ok = function () {
+    $modalInstance.close();
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
+})
