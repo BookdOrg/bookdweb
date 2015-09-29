f28c554054a769f002a68ca743959652f1354de3
Khalil <knb6@njit.edu>
2015-09-29 11:47:32 -0400

    Fix for query.location breaking with location is an object not a string.

--- public/javascripts/controllers/nav-controller.js
+++ public/javascripts/controllers/nav-controller.js
@@ -27,6 +27,15 @@ function($scope, auth, $state,businessFactory,$rootScope,$geolocation,$http,loca
         enableHighAccuracy: true
     });
     $scope.myPosition = $geolocation.position;
+
+    /**
+     *
+     * Watch for when the users location changes, make a call to the google maps api to
+     * get information about the users current location.
+     *
+     * Auto populate that information in the query location object, to be displayed int he navbar.
+     *
+     */
     $scope.$watch('myPosition.coords.latitude',function(newVal,oldVal){
         $scope.loadingLocation = true;
         if(newVal !== oldVal){
@@ -45,11 +54,29 @@ function($scope, auth, $state,businessFactory,$rootScope,$geolocation,$http,loca
         }
     })
 
-
+    /**
+     *
+     * Concatenates the query term and query location entered in the Navbar
+     * to create the query string being sent to the Places API on the backend.
+     *
+     * If the typeOf the queryLocation is a string, (User typed it in) then concatenate
+     * query.location with query.term
+     *
+     * If the typeOf the queryLocation is !string (an Object) then concatenate query.location.vicinity
+     * with query.term
+     *
+     * @param query - Object with term and location properties. Location will either be a string or an object.
+     */
 
   $scope.search = function(query){
     $scope.fetchingQuery = true;
-    var formattedQuery = query.term + " " + query.location;
+      var formattedQuery;
+    if(typeof query.location == "string"){
+        formattedQuery = query.term + " " + query.location;
+    }else{
+        formattedQuery = query.term + " " + query.location.vicinity;
+    }
+
     businessFactory.search(formattedQuery)
         .then(function(data){
             $scope.fetchingQuery = false;
