d525458a895d073a3f6912c0fd4e52a364e297a3
jonfor <jonforscher@gmail.com>
2015-10-07 19:04:06 -0400

    Less offensive colors

--- public/javascripts/controllers/landing-controller.js
+++ public/javascripts/controllers/landing-controller.js
@@ -2,8 +2,5 @@ angular.module('cc.landing-controller',[])
     .controller('landingCtrl', [
       '$scope',
       function($scope){
-        $scope.focus = function() {
-          //$('#shop-search').focus();
-          angular.element('#shop-search').focus();
-        }
+
       }]);
