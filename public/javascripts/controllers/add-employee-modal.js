826aa9c6f5a758e794328013baaa6641d7e5c468
Jonfor <jonforscher@gmail.com>
2015-11-28 18:28:21 -0500

    Business modals into separate files

---
+++ public/javascripts/controllers/add-employee-modal.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Jonfor on 11/28/15.
+ */
+module.exports = function ($scope, $modalInstance, businessFactory, user) {
+
+    $scope.create = function (id) {
+        var business = businessFactory.business;
+        var newEmployee = {
+            businessId: business.info._id,
+            employeeId: id
+        };
+        businessFactory.addEmployee(newEmployee);
+        $modalInstance.close();
+    };
+
+    /**
+     * Called when adding a new employee to a business.
+     * Search for the employee by email and display the relevant result.
+     * @param email - Email address of employee to search for.
+     */
+    $scope.findEmployee = function (email) {
+        $scope.searched = false;
+        user.search(email).then(function (data) {
+            $scope.searched = true;
+            $scope.employee = data;
+        }, function (error) {
+            $scope.searched = true;
+        });
+    };
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+};
---
+++ public/javascripts/controllers/add-service-modal.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Jonfor on 11/28/15.
+ */
+module.exports = function ($scope, $modalInstance, businessFactory, business) {
+
+    $scope.business = business;
+
+    // $scope.service = {
+    //   employees: []
+    // }
+    $scope.serviceEmployees = [];
+    $scope.settings = {
+        displayProp: 'name',
+        idProp: '_id',
+        externalIdProp: '_id',
+        smartButtonMaxItems: 3,
+        smartButtonTextConverter: function (itemText, originalItem) {
+            return itemText;
+        }
+    };
+    $scope.ok = function (service) {
+        service.businessId = business._id;
+        service.employees = _.pluck($scope.serviceEmployees, '_id');
+        businessFactory.addService(service);
+        $modalInstance.close();
+    };
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+};
