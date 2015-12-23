3bc7705fbe85d83f2291233fc4a04a520ac56209
Khalil <knb6@njit.edu>
2015-12-22 19:55:42 -0500

    multiple map markers is not working right now -- ignore where i set $scope.center in both the users location and in the generate markers loop. put it there while testing and decided to leave it for now

--- public/javascripts/controllers/main-controller.js
+++ public/javascripts/controllers/main-controller.js
@@ -6,7 +6,34 @@ module.exports = function ($scope, businessFactory, $controller) {
     navViewModel.showSearch(true);
     $scope.$watch('currLocation', function (newVal, oldVal) {
         if (newVal) {
-            $scope.map = {center: {latitude: newVal.latitude, longitude: newVal.longitude}, zoom: 12};
+            //$scope.map = {center: {latitude: newVal.latitude, longitude: newVal.longitude}, zoom: 12};
+            $scope.center = newVal.latitude + ',' + newVal.longitude;
         }
+    });
+    var vm = this;
+    vm.positions = [];
+    var generateMarkers = function(businesses) {
+        vm.positions = [];
+        var numMarkers = businesses.length;
+        for (var i = 0; i < numMarkers; i++) {
+            var lat = businesses[i].geometry.location.lat;
+            var lng = businesses[i].geometry.location.lng;
+            $scope.center = businesses[i].geometry.location.lat+','+businesses[i].geometry.location.lng;
+            vm.positions.push({lat:lat, lng:lng});
+        }
+        console.log("vm.positions", vm.positions);
+    };
+    $scope.$watchCollection('businesses', function (newVal, oldVal) {
+        if (newVal !== oldVal) {
+            generateMarkers(newVal);
+
+        }
+        //if (!newVal && oldVal) {
+        //    $scope.selectedQuery = oldVal;
+        //}
+        //
+        //if ($scope.query.geometry) {
+        //    $scope.center = $scope.query.geometry.location.lat() + ',' + $scope.query.geometry.location.lng();
+        //}
     });
 };
