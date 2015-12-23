a93afde2a01b858da9678aff84439c25b6e7006e
Khalil <knb6@njit.edu>
2015-12-23 01:25:29 -0500

    generate map marker for result OR create map marker based on curr location depending on context

--- public/javascripts/controllers/main-controller.js
+++ public/javascripts/controllers/main-controller.js
@@ -1,17 +1,9 @@
-module.exports = function ($scope, businessFactory, $controller) {
+module.exports = function ($scope, businessFactory, $controller,$rootScope) {
     $scope.businesses = businessFactory.businesses;
-    var navViewModel = $scope.$new();
-    $controller('NavCtrl', {$scope: navViewModel});
 
-    navViewModel.showSearch(true);
-    $scope.$watch('currLocation', function (newVal, oldVal) {
-        if (newVal) {
-            //$scope.map = {center: {latitude: newVal.latitude, longitude: newVal.longitude}, zoom: 12};
-            $scope.center = newVal.latitude + ',' + newVal.longitude;
-        }
-    });
     var vm = this;
     vm.positions = [];
+
     var generateMarkers = function(businesses) {
         vm.positions = [];
         var numMarkers = businesses.length;
@@ -23,17 +15,17 @@ module.exports = function ($scope, businessFactory, $controller) {
         }
         console.log("vm.positions", vm.positions);
     };
-    $scope.$watchCollection('businesses', function (newVal, oldVal) {
-        if (newVal !== oldVal) {
-            generateMarkers(newVal);
+    generateMarkers($scope.businesses);
+    var navViewModel = $scope.$new();
+    $controller('NavCtrl', {$scope: navViewModel});
 
+    navViewModel.showSearch(true);
+    $scope.$watch('currLocation',function(newVal,oldVal){
+        if(newVal){
+            $scope.position = $rootScope.currLocation;
+            if($scope.businesses.length == 0){
+                $scope.center = $scope.position.latitude + ',' + $scope.position.longitude;
+            }
         }
-        //if (!newVal && oldVal) {
-        //    $scope.selectedQuery = oldVal;
-        //}
-        //
-        //if ($scope.query.geometry) {
-        //    $scope.center = $scope.query.geometry.location.lat() + ',' + $scope.query.geometry.location.lng();
-        //}
     });
 };
