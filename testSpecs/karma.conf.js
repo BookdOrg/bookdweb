5f2fcbaa2784d2a8d975de2f1e3538da14b147bd
Jonfor <jonforscher@gmail.com>
2015-12-25 13:30:25 -0500

    Cleanup

--- testSpecs/karma.conf.js
+++ testSpecs/karma.conf.js
@@ -5,7 +5,7 @@ module.exports = function(config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
+      basePath: '../),
 
 
     // frameworks to use
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-        'bower_components/angular/angular.js',
+        '../bower_components/angular/angular.js',
         'bower_components/angular-mocks/angular-mocks.js',
         'bower_components/jquery/dist/jquery.js',
         'bower_components/angular-ui-router/release/angular-ui-router.js',
