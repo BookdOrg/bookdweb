99b5783a232503842b5a85f8cf752d8dcfba1ace
Khalil <knb6@njit.edu>
2015-12-27 15:55:31 -0500

    google photo on profile page

--- public/javascripts/controllers/profile-controller.js
+++ public/javascripts/controllers/profile-controller.js
@@ -8,6 +8,12 @@ module.exports = function ($scope, auth, userFactory, $location, $sce, FileUploa
     $scope.rate = 2.5;
     userFactory.get($stateParams.id).then(function (data) {
         $scope.user = data.user;
+        if (data.user.provider === 'google_plus') {
+            userFactory.getGooglePhoto(data.user.providerId)
+                .then(function (response) {
+                    $scope.user.photo = response.image.url.replace('sz=50', 'sz=200');
+                });
+        }
     });
 
     $scope.facebookApi = facebookApi;
