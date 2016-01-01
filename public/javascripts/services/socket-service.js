c50a0af928a6eccaa0c4c567999d17c911489059
Jonfor <jonforscher@gmail.com>
2015-12-31 19:09:54 -0500

    Being organized is fun

--- public/javascripts/services/socket-service.js
+++ public/javascripts/services/socket-service.js
@@ -4,27 +4,34 @@
  */
 module.exports = function ($rootScope) {
     var socket = io.connect('//localhost:3001');
-    return {
-        on: function (eventName, callback) {
-            socket.on(eventName, function () {
-                var args = arguments;
-                $rootScope.$apply(function () {
+
+    function on(eventName, callback) {
+        socket.on(eventName, function () {
+            var args = arguments;
+            $rootScope.$apply(function () {
+                callback.apply(socket, args);
+            });
+        });
+    }
+
+    function emit(eventName, data, callback) {
+        socket.emit(eventName, data, function () {
+            var args = arguments;
+            $rootScope.$apply(function () {
+                if (callback) {
                     callback.apply(socket, args);
-                });
+                }
             });
-        },
-        emit: function (eventName, data, callback) {
-            socket.emit(eventName, data, function () {
-                var args = arguments;
-                $rootScope.$apply(function () {
-                    if (callback) {
-                        callback.apply(socket, args);
-                    }
-                });
-            });
-        },
-        disconnect: function () {
-            socket.disconnect();
-        }
+        });
+    }
+
+    function disconnect() {
+        socket.disconnect();
+    }
+
+    return {
+        on: on,
+        emit: emit,
+        disconnect: disconnect
     };
 };
