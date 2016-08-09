82aac066e1b9533ea38f4042717666e0baf2d4ea
Khalil <knb6@njit.edu>
2016-08-09 19:56:46 -0400

    i broke facebook login because idk how to copy pasta an auth key 'rite'.

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ var shared = {
     CLOUDINARY_Default: "https://res.cloudinary.com/dvvtn4u9h/image/upload/c_thumb,h_300,w_300/v1432411957/profile/placeholder.jpg",
     CLOUDINARY_BASE: "https://res.cloudinary.com/dvvtn4u9h/image/upload/c_thumb,h_300,w_300/v",
     facebookApi: "https://graph.facebook.com/",
-    oAuthKey: "mPBNkFFrqBA1L6cT0C7og9"
+    oAuthKey: "mPBNkFFrqBA1L6cT0C7og9-xdQM"
 };
 
 //
