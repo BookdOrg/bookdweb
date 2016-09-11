d58175472ccc9a7f626921e487817f9da99facae
Khalil <knb6@njit.edu>
2016-09-11 12:56:49 -0400

    adding in redis for socket scaling. load balancing and sticky sessions

--- routes/sockets.js
+++ routes/sockets.js
@@ -45,6 +45,8 @@ if (process.env.NODE_ENV === 'development') {
     server = require('https').createServer(options, app);
 }
 var io = require('socket.io')(server);
+var redis = require('socket.io-redis');
+io.adapter(redis({host: process.env.devHost, port: 6379}));
 var wellknown = require('nodemailer-wellknown');
 var config = wellknown('Zoho');
 // create reusable transporter object using SMTP transport
