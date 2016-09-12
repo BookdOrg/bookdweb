4a988081dca2a0b857827e99639699dc2d2bb162
Khalil <knb6@njit.edu>
2016-09-11 20:32:08 -0400

    out for good now

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -3,4 +3,3 @@ node_modules/bower/bin/bower install
 node_modules/gulp/bin/gulp.js ng-config
 node_modules/gulp/bin/gulp.js minify-cssOnce
 node_modules/gulp/bin/gulp.js browserifyProdOnce
-pm2 reload all
