f31709563480537a75482f4e12fcdeea26833e47
kbrown87 <kbrown87@its.jnj.com>
2018-07-21 20:14:59 -0400

    Migration from oauth io auth sign to factory.

--- public/javascripts/controllers/auth-controller.js
+++ public/javascripts/controllers/auth-controller.js
@@ -1,5 +1,5 @@
 module.exports = function ($scope, $state, auth, $uibModalInstance, modalType, state, socketService, $rootScope,
-                           userFactory, notificationFactory, tier, context) {
+                           userFactory, notificationFactory, tier, context, facebookService) {
     $scope.user = {};
     $scope.tabs = [
         {
@@ -19,7 +19,7 @@ module.exports = function ($scope, $state, auth, $uibModalInstance, modalType, s
     if (context === "businessRegister") {
         $scope.contextMessage = true;
     }
-    var onlineData = {
+	let onlineData = {
         user: '',
         location: {}
     };
@@ -27,79 +27,65 @@ module.exports = function ($scope, $state, auth, $uibModalInstance, modalType, s
         $scope.error = null;
         $scope.user = {};
     };
-    //TODO handle all the possible error cases
+	//TODO handle all the possible error cases
     $scope.facebookLogin = function () {
-        OAuth.popup('facebook', {cache: true})
-            .done(function (result) {
-                //console.log(result);
-                result.get('/me?fields=id,name,picture.type(large),email')
-                    .done(function (response) {
-                        var user = {
-                            'username': response.email,
-                            'provider': result.provider
-                        };
-                        auth.logIn(user, response.picture.data.url)
-                            .then(function () {
-                                onlineData.user = $rootScope.currentUser._id;
-                                socketService.emit('online', onlineData);
-                                $state.go(state, {tier: tier});
-                                getNotifications();
-                                $uibModalInstance.close();
-                            }, function (error) {
-                                $scope.error = error.message;
-                            });
-                    })
-                    .fail(function (err) {
-                        //console.log(err);
-                        //handle error with err
-                    });
-            })
-            .fail(function (err) {
-                //console.log(err);
-                //handle error with err
-            });
+	    facebookService.checkFaceBookLoginStatus()
+		    .then(function (response) {
+				    if (response.authResponse)
+					    let user = {
+						    username: response.email,
+						    provider: 'facebook'
+					    };
+				    auth.logIn(user, response.picture.data.url)
+					    .then(function () {
+						    onlineData.user = $rootScope.currentUser._id;
+						    socketService.emit('online', onlineData);
+						    $state.go(state, {tier: tier});
+						    getNotifications();
+						    $uibModalInstance.close();
+					    }, function (error) {
+						    $scope.error = error.message;
+					    });
+			    },
+			    function (error) {
+				    $scope.error = error;
+			    });
     };
     $scope.facebookSignup = function () {
-        OAuth.popup('facebook', {cache: true})
-            .done(function (result) {
-                result.get('/me?fields=id,name,picture.type(large),email')
-                    .done(function (response) {
-                        var firstLast = response.name.split(' ', 2);
-                        var firstName = firstLast[0];
-                        var lastName = firstLast[1];
-                        var user = {
-                            'username': response.email,
-                            'name': response.name,
-                            'firstName': firstName,
-                            'lastName': lastName,
-                            'provider': result.provider,
-                            'providerId': response.id
-                        };
-                        var profilePicture = response.picture.data.url;
-                        auth.register(user, profilePicture)
-                            .then(function () {
-                                onlineData.user = $rootScope.currentUser._id;
-                                socketService.emit('online', onlineData);
-                                $state.go(state, {tier: tier});
-                                $rootScope.currentUser.notifications = [];
-                                $uibModalInstance.close();
-                            }, function (error) {
-                                $scope.error = error.message;
-                            });
-                    })
-                    .fail(function (err) {
-                        //console.log(err);
-                    });
-            })
-            .fail(function (err) {
-                //console.log(err);
-            });
+	    facebookService.checkFaceBookLoginStatus()
+		    .then(function (response) {
+				    let firstLast = response.name.split(' ', 2);
+				    let firstName = firstLast[0];
+				    let lastName = firstLast[1];
+				    let user = {
+					    'username': response.email,
+					    'name': response.name,
+					    'firstName': firstName,
+					    'lastName': lastName,
+					    'provider': 'facebook',
+					    'providerId': response.id
+				    };
+				    // let profilePicture = response.picture.data.url;
+				    auth.logIn(user, response.picture.data.url)
+					    .then(function () {
+						    onlineData.user = $rootScope.currentUser._id;
+						    socketService.emit('online', onlineData);
+						    $state.go(state, {tier: tier});
+						    getNotifications();
+						    $uibModalInstance.close();
+					    }, function (error) {
+						    $scope.error = error;
+					    });
+			    },
+			    function (error) {
+				    $scope.error = error;
+			    });
     };
     /**
      * Register via Bookd
      */
     $scope.register = function () {
-        var user = {
+	    let user = {
             'username': $scope.user.email,
             'name': $scope.user.firstName + ' ' + $scope.user.lastName,
             'firstName': $scope.user.firstName,
@@ -122,7 +108,7 @@ module.exports = function ($scope, $state, auth, $uibModalInstance, modalType, s
      * Login via Bookd
      */
     $scope.logIn = function () {
-        var user = {
+	    let user = {
             'username': $scope.user.email,
             'password': $scope.user.password,
             'provider': 'bookd'
@@ -143,7 +129,7 @@ module.exports = function ($scope, $state, auth, $uibModalInstance, modalType, s
         $uibModalInstance.dismiss('close');
     };
 
-    var getNotifications = function () {
+	let getNotifications = function () {
         notificationFactory.getNotifications().then(
             function (data) {
                 $rootScope.currentUser.notifications = data;
--- public/javascripts/services/auth-factory.js
+++ public/javascripts/services/auth-factory.js
@@ -150,7 +150,53 @@ module.exports = function ($http, $window, $rootScope, $state, $q, socketService
                 }, function (err) {
                     throw err.data;
                 });
-        }
+        },
+	    watchLoginChange: function () {
+
+		    var _self = this;
+
+		    FB.Event.subscribe('auth.authResponseChange', function (res) {
+
+			    if (res.status === 'connected') {
+
+				    /*
+					 The user is already logged,
+					 is possible retrieve his personal info
+					*/
+				    _self.getFbUserInfo();
+
+				    /*
+					 This is also the point where you should create a
+					 session for the current user.
+					 For this purpose you can use the data inside the
+					 res.authResponse object.
+					*/
+
+			    }
+			    else {
+
+				    /*
+					 The user is not logged to the app, or into Facebook:
+					 destroy the session on the server.
+					*/
+
+			    }
+
+		    });
+
+	    },
+	    getFbUserInfo: function () {
+
+		    var _self = this;
+
+		    FB.api('/me', function (res) {
+			    console.log(res);
+			    // $rootScope.$apply(function() {
+			    //    $rootScope.user = _self.user = res;
+			    // });
+		    });
+
+	    }
     };
 
     return auth;
